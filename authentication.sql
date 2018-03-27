6cb3e3bde07a8143dccf49704173e86df2a78f7c
wangdi <wangdi10@xiaomi.com>
2018-03-27 16:48:13 +0800

    表结构

--- authentication.sql
+++ authentication.sql
@@ -1,30 +1,15 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : 172.168.1.100
-Source Server Version : 50721
-Source Host           : 172.168.1.100:3306
-Source Database       : authentication
-
-Target Server Type    : MYSQL
-Target Server Version : 50721
-File Encoding         : 65001
-
-Date: 2018-03-26 22:36:32
-*/
-
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for right
+-- Table structure for permission
 -- ----------------------------
-DROP TABLE IF EXISTS `right`;
-CREATE TABLE `right` (
-  `right_id` bigint(20) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `permission`;
+CREATE TABLE `permission` (
+  `permission_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `key` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   `uri` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`right_id`)
+  PRIMARY KEY (`permission_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -39,14 +24,14 @@ CREATE TABLE `role` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for role_right
+-- Table structure for role_permission
 -- ----------------------------
-DROP TABLE IF EXISTS `role_right`;
-CREATE TABLE `role_right` (
-  `role_right_id` bigint(20) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `role_permission`;
+CREATE TABLE `role_permission` (
+  `role_permission_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `role_id` bigint(20) DEFAULT NULL,
-  `right_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`role_right_id`)
+  `permission_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`role_permission_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -54,23 +39,24 @@ CREATE TABLE `role_right` (
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `userId` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `login_name` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `salt` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `locked` bit(1) DEFAULT b'0',
-  PRIMARY KEY (`userId`)
+  PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for user_right
+-- Table structure for user_role_permission
 -- ----------------------------
-DROP TABLE IF EXISTS `user_right`;
-CREATE TABLE `user_right` (
-  `user_right_id` bigint(20) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `user_role_permission`;
+CREATE TABLE `user_role_permission` (
+  `user_role_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `user_id` bigint(20) DEFAULT NULL,
-  `right_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`user_right_id`)
+  `role_id` bigint(20) DEFAULT NULL,
+  `permission_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`user_role_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
