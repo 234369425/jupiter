103efb5648566ac31ae4b06eb7ed4b2441ef5a2a
wangdi <wangdi10@xiaomi.com>
2018-05-18 11:04:44 +0800

    结构变更

--- src/main/java/com/beheresoft/security/Application.java
+++ src/main/java/com/beheresoft/security/Application.java
@@ -3,15 +3,11 @@ package com.beheresoft.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author Aladi
  */
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@ComponentScan({"com.beheresoft.security.repository", "com.beheresoft.security.service",
-        "com.beheresoft.security.realm", "com.beheresoft.security.controller"
-})
 public class Application {
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(Application.class);
