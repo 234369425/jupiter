008d8a72868da0fdd1181404b95812e70cd9440f
Aladi <234369425@qq.com>
2018-06-07 23:03:41 +0800

    change sql

--- authentication.sql
+++ authentication.sql
@@ -1,16 +1,55 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 172.168.1.1
+Source Server Version : 50722
+Source Host           : 172.168.1.1:3306
+Source Database       : authentication
+
+Target Server Type    : MYSQL
+Target Server Version : 50722
+File Encoding         : 65001
+
+Date: 2018-06-07 23:02:49
+*/
+
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for permission
+-- Table structure for application
 -- ----------------------------
-DROP TABLE IF EXISTS `permission`;
-CREATE TABLE `permission` (
-  `permission_id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `key` varchar(255) DEFAULT NULL,
+DROP TABLE IF EXISTS `application`;
+CREATE TABLE `application` (
+  `app_id` bigint(20) NOT NULL,
+  `app_name` varchar(255) DEFAULT NULL,
+  `app_key` varchar(255) DEFAULT NULL,
+  `app_secret` varchar(255) DEFAULT NULL,
+  `available` bit(1) DEFAULT NULL,
+  PRIMARY KEY (`app_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of application
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for resource
+-- ----------------------------
+DROP TABLE IF EXISTS `resource`;
+CREATE TABLE `resource` (
+  `resource_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `parent_id` bigint(20) DEFAULT NULL,
+  `app_name` varchar(255) DEFAULT NULL,
+  `type` tinyint(255) DEFAULT NULL,
+  `perm_key` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   `uri` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`permission_id`)
+  PRIMARY KEY (`resource_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of resource
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for role
@@ -18,21 +57,31 @@ CREATE TABLE `permission` (
 DROP TABLE IF EXISTS `role`;
 CREATE TABLE `role` (
   `role_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `app_name` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   `descirb` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`role_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of role
+-- ----------------------------
+INSERT INTO `role` VALUES ('1', 'jupiter', 'superAdmin', '系统管理员');
+
+-- ----------------------------
+-- Table structure for role_resource
+-- ----------------------------
+DROP TABLE IF EXISTS `role_resource`;
+CREATE TABLE `role_resource` (
+  `role_resource_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `role_id` bigint(20) DEFAULT NULL,
+  `resource_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`role_resource_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for role_permission
+-- Records of role_resource
 -- ----------------------------
-DROP TABLE IF EXISTS `role_permission`;
-CREATE TABLE `role_permission` (
-  `role_permission_id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `role_id` bigint(20) DEFAULT NULL,
-  `permission_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`role_permission_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for user
@@ -40,6 +89,7 @@ CREATE TABLE `role_permission` (
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `user_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `app_name` varchar(255) DEFAULT NULL,
   `login_name` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `salt` varchar(255) DEFAULT NULL,
@@ -50,15 +100,21 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for user_role_permission
+-- Records of user
 -- ----------------------------
-DROP TABLE IF EXISTS `user_role_permission`;
-CREATE TABLE `user_role_permission` (
+INSERT INTO `user` VALUES ('-1', 'jupiter', 'admin', 'a9c02c7e989e121ee0aefd5f9e44751d', '03a730292cb34378e8b7ed4813ee23da', '系统管理员', '234@q.com', '\0');
+
+-- ----------------------------
+-- Table structure for user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `user_role`;
+CREATE TABLE `user_role` (
   `user_role_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `user_id` bigint(20) DEFAULT NULL,
   `role_id` bigint(20) DEFAULT NULL,
-  `permission_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`user_role_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `authentication`.`user` (`user_id`, `login_name`, `password`, `salt`, `name`, `email`, `locked`) VALUES ('-1', 'admin', 'a9c02c7e989e121ee0aefd5f9e44751d', '03a730292cb34378e8b7ed4813ee23da', '系统管理员', '234@q.com', b'0');
+-- ----------------------------
+-- Records of user_role
+-- ----------------------------
